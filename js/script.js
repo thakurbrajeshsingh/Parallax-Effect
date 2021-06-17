b264e1ade1b97bea67a68e0e9953fc9eaa1d0d16
BRAJESH997 <40537685+BRAJESH997@users.noreply.github.com>
2021-06-18 02:09:05 +0530

    Parallex Effect Added

--- js/script.js
+++ js/script.js
@@ -12,7 +12,10 @@ window.addEventListener('scroll',function(){
     let value = window.scrollY;
 
     text.style.top= 50+ value * -0.5 + '%';
-    bird1.style.top=50+ value * -1.5 + 'px';
-    // text.style.top=50+ value * -0.5 + "%";
-
+    bird1.style.top=value * -1.5 + 'px';
+    bird2.style.left=value * -5 + 'px';
+    btn.style.marginTop = value * 1.5 + 'px';
+    rocks.style.top = value *  + -0.12 + 'px';
+    forest.style.top = value *  + 0.25 + 'px';
+    header.style.top = value *  + -0.5 + 'px';
 })
